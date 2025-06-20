f64f555bdbc8e9949e6eb35fb7616de46786e3d5
GS-Luiz-Gustavo <ti.automacao01@gscsc.com.br>
2025-06-20 13:37:13 -0300

    pequenos ajustes

--- GS/Extratos_Bancarios/main.py
+++ GS/Extratos_Bancarios/main.py
@@ -1,16 +1,15 @@
-# from modelos.SPX_VISON.main import spx_vision
-# from modelos.CAIXA.app import CAIXA
 from modelos.AIRBI.app import AIRBI
-# from modelos.BANESTES_RPL.app import BANESTES_RPL
-# from modelos.GRAFENO.app import GRAFENO
-# from modelos.GRAFENO_HITEC.app import GRAFENO_HITEC
-# from modelos.HITEC.app import HITEC
-# from modelos.ITAU.app import ITAU
-# from modelos.ITAU_VISION.app import ITAU_VISION
-# from modelos.SANTANDER.app import SANTANDER
-# from modelos.SANTANDER_SING.app import SANTANDER_SING
-# from modelos.CEF_FOLK.app import CEF_FOLK
-# from modelos.CORA_GS_FACILITIES.app import CORA_GS_FACILITIES
+from modelos.BANESTES_RPL.app import BANESTES_RPL
+from modelos.BB_CONDO_PREST.app import bb_condoprest
+from modelos.BRADESCO_HISYSTEM.app import bradesco_hisystem
+from modelos.caixa.app import CAIXA
+from modelos.CEF_FOLK.app import CEF_FOLK
+from modelos.CORA_GS_FACILITIES.app import CORA_GS_FACILITIES
+from modelos.grafeno.app import GRAFENO
+from modelos.GRAFENO_HITEC.app import GRAFENO_HITEC
+from modelos.HITEC.app import HITEC
+from modelos.hitec_37_spx.app import hitec_37_spx
+from modelos.
 
 
 # path = 'modelos/SPX_VISON/SPX VISON.pdf'
