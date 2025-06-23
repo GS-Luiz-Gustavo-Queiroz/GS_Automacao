bd060640d3817680bbff996f5319697b0da795c7
GS-Luiz-Gustavo <ti.automacao01@gscsc.com.br>
2025-06-23 13:49:36 -0300

    Concluído NFs Fortaleza

--- GS/Operacoes em PDF/Notas Fiscais/Fortaleza/main.py
+++ GS/Operacoes em PDF/Notas Fiscais/Fortaleza/main.py
@@ -1,17 +1,24 @@
-import os
+from report_functions import salva_relatorio
+from datetime import datetime
 from PyPDF2 import PdfReader
+from time import time
+from tqdm import tqdm
+import os
 
-def main() -> None:
+
+def main() -> int:
+    tot_pags: int = 0
+
     files = [file for file in os.listdir() if '.pdf' in file.lower()]
-
-    for file in files:
+    for file in tqdm(files):
         with open(file, 'rb') as file_b:
             pdf = PdfReader(file_b).pages[0]
             rows = pdf.extract_text().split('\n')
-
+            tot_pags += len(PdfReader(file_b).pages)
         if rows[0] == 'Número da':
             # Modelo 1
-            num_nf = rows[1][:4]
+            cnpj = ' ERRO '
+            num_nf = ''.join(i for i in rows[1].split()[0] if i.isnumeric())
             for row in rows:
                 if 'Complemento:' in row:
                     nome = row[12:].strip()
@@ -21,32 +28,28 @@ def main() -> None:
             primeiro = True
             for i, row in enumerate(rows):
                 if row == 'NFS-e':
-                    num_nf = rows[i+1]
+                    num_nf = rows[i + 1]
                 if row == 'Razão Social/Nome':
                     if primeiro:
                         primeiro = False
                     else:
-                        nome = rows[i+1]
+                        nome = rows[i + 1]
+                        cnpj = ''.join([char for char in rows[i + 3] if char.isnumeric()])
                         break
-        elif rows[0] == 'PREFEITURA MUNICIPAL DE FORTALEZA':
-            # modelo 3
-            for i, row in enumerate(rows):
-                if row == 'NFS-e':
-                    num_nf = rows[i+1]
-                    break
-            for row in rows:
-                if 'Regime especial Tributação' in row:
-                    nome = row[26:]
-                    break
         else:
             continue
-
-        os.rename(file, f'NF {num_nf} - {nome}.pdf')
+        os.rename(file, f'NF {nome}-{cnpj}.pdf')
+    return tot_pags
 
 
 if __name__ == '__main__':
     try:
-        main()
+        st = time()
+        n_pags = main()
+        tempo_exec = time() - st
+        data = datetime.now().strftime("%d/%m/%Y")
+        salva_relatorio([[data, 'NFs Fortaleza', n_pags, tempo_exec]])
+
     except Exception as e:
         print(e)
         input()
