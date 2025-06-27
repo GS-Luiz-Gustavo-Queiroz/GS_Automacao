32e42c80e89e5887eeb68b6e3c988b164b62ebc7
joaquim oliveira <154386267+joaquimoliveira22@users.noreply.github.com>
2025-06-27 11:17:17 -0300

    atualizando atualizar pastas

--- GS/Extratos_Bancarios/main.py
+++ GS/Extratos_Bancarios/main.py
@@ -1,23 +1,21 @@
-try:
-    from configs.processa_arquivos import processa_arquivos
-    from configs.utils import get_grupos_dir
-    from configs.arquivo import Arquivo
-    from configs.atualizar_pastas import atualizar_pastas, get_creds
-    from tqdm import tqdm
-    import pandas as pd
-    import os
-    import warnings
-    warnings.simplefilter("ignore", UserWarning)
-    warnings.simplefilter(action='ignore', category=FutureWarning)
-except Exception as e:
-    print(e)
-    input()
+
+from configs.processa_arquivos import processa_arquivos
+from configs.utils import get_grupos_dir
+from configs.arquivo import Arquivo
+from configs.atualizar_pastas import atualizar_pastas, get_creds
+from tqdm import tqdm
+import pandas as pd
+import os
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 
 def main() -> None:
-    atualizar_pastas(get_creds())
     # Caminho para a pasta 'GRUPOS'.
     path = get_grupos_dir()
+    atualizar_pastas(get_creds(), path)
     if not os.path.exists(path):
         raise Exception('Caminho para a pasta "GRUPOS" não encontrado.')
 
