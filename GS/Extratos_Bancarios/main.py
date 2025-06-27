3cd12058090bcc8bb8a8842b926bf18a852571b3
joaquim oliveira <154386267+joaquimoliveira22@users.noreply.github.com>
2025-06-27 14:21:22 -0300

    atualização do main

--- GS/Extratos_Bancarios/main.py
+++ GS/Extratos_Bancarios/main.py
@@ -1,10 +1,13 @@
 
 from configs.processa_arquivos import processa_arquivos
+from configs.utils import list_all_files, salva_erros, salva_relatorio
 from configs.utils import get_grupos_dir
 from configs.arquivo import Arquivo
 from configs.atualizar_pastas import atualizar_pastas, get_creds
+from datetime import datetime
 from tqdm import tqdm
 import pandas as pd
+import time
 import os
 import warnings
 warnings.simplefilter("ignore", UserWarning)
@@ -14,14 +17,22 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 def main() -> None:
     # Caminho para a pasta 'GRUPOS'.
+    st = time.time()
     path = get_grupos_dir()
     atualizar_pastas(get_creds(), path)
     if not os.path.exists(path):
         raise Exception('Caminho para a pasta "GRUPOS" não encontrado.')
-
+    files = list_all_files([path])
+    data = datetime.now().strftime("%d/%m/%Y")
+    exec_time = time.time() - st
+    salva_relatorio([[data, 'Extratos Bancários', len(files), exec_time]])
+    arquivos_processados = salva_relatorio
     df = processa_arquivos(path)
     df.to_excel('Arquivos_formatados.xlsx', index=False)
-
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    pd.DataFrame(arquivos_processados).to_excel('logs/arquivos_processados.xlsx', index=False)
+    pd.DataFrame('logs/arquivos_encontrados.xlsx', index=False)
 
 
 if __name__ == "__main__":
