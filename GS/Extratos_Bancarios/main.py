9e90e7d096752102d98c2cb7e6ffa17ad4a71a63
joaquim oliveira <154386267+joaquimoliveira22@users.noreply.github.com>
2025-06-30 13:56:57 -0300

    atualizando main e processa_arquivos

--- GS/Extratos_Bancarios/main.py
+++ GS/Extratos_Bancarios/main.py
@@ -18,26 +18,32 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 def main() -> None:
     # Caminho para a pasta 'GRUPOS'.
     st = time.time()
+    erros = []
     path = get_grupos_dir()
-    atualizar_pastas(get_creds(), path)
     if not os.path.exists(path):
         raise Exception('Caminho para a pasta "GRUPOS" não encontrado.')
+    arquivos_processados = processa_arquivos(path)
+    atualizar_pastas(get_creds(), path)
+    print(path)
     files = list_all_files([path])
+    print(files)
     data = datetime.now().strftime("%d/%m/%Y")
     exec_time = time.time() - st
-    salva_relatorio([[data, 'Extratos Bancários', len(files), exec_time]])
-    arquivos_processados = salva_relatorio
-    df = processa_arquivos(path)
-    df.to_excel('Arquivos_formatados.xlsx', index=False)
+    print(data)
+    print(exec_time)
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    pd.DataFrame(arquivos_processados).to_excel('logs/arquivos_processados.xlsx', index=False)
-    pd.DataFrame('logs/arquivos_encontrados.xlsx', index=False)
+    relatorio = ([[data, 'Extratos Bancários', 'Arquivos_formatados.xlsx', len(files), arquivos_processados, exec_time, erros]])
+    print(relatorio)
+    # salva_relatorio(relatorio)
+    pd.DataFrame(relatorio, columns=['Data', 'Tipo', 'Arquivo', 'Qtd Arquivos', 'arquivos_processados', 'Tempo Execução', 'Erros']).to_excel(
+    'logs/arquivos_processados.xlsx', index=False)
+main()
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        print(e)
-        input()
+# if __name__ == "__main__":
+#     try:
+#         main()
+#     except Exception as e:
+#         print(e)
+#         input()
