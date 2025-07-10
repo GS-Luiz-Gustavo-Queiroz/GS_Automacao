f9d928809c5a29815c54d056342401cb2c85e6b4
José Eduardo Soares de Lima Nogueira <ti.automacao03@gscsc.com.br>
2025-07-10 17:35:53 -0300

    Resolvido

--- GS/Contas a Pagar/main.py
+++ GS/Contas a Pagar/main.py
@@ -192,7 +192,7 @@ class Aut:
                     nome = nome[:nome.rfind('.')]  # Remove a extensão do nome do arquivo.
                     # Define o nome junto do codigo do CPG, nome antigo e o vencimento.
                     # novo_nome = f'{cod_cpg} - {nome} - {vencimento}{extensao}'
-                    novo_nome = f'{grupo} - R$ {valor} - {fornecedor} - {num_nota}{extensao}'.replace("/", "")
+                    novo_nome = f'{grupo} - R$ {valor} - {fornecedor} - {data[3:]}{extensao}'.replace("/", "")
                     new_path = f'{dir_ven}/{novo_nome}'  # Junta o novo nome com o diretório de destino do vencimento.
                     new_path = os.path.join(new_path)
                     new_path = '\\\\?\\' + os.path.abspath(new_path)
@@ -205,7 +205,7 @@ class Aut:
                         # Então vai ser tentado apenas diminuir o tamanho do arquivo.
                         # O i cria um id para os arquivos, para não haver arquivos de mesmo nome.
                         i = len(os.listdir(dir_ven))
-                        novo_nome = f'{dir_ven}/R$ {valor} - {fornecedor} - {num_nota}({i}){extensao}'.replace("/", "")
+                        novo_nome = f'{dir_ven}/{grupo} - R$ {valor} - {fornecedor} - {data[3:]}({i}){extensao}'.replace("/", "")
                         new_path = os.path.join(novo_nome)
                         new_path = '\\\\?\\' + os.path.abspath(new_path)
                         try:
