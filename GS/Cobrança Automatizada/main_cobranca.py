75a3c3460bf821fe2a04bd6ccd2aca20de3f4d80
José Eduardo Soares de Lima Nogueira <ti.automacao03@gscsc.com.br>
2025-07-11 16:54:52 -0300

    Consertado

--- GS/Cobrança Automatizada/main_cobranca.py
+++ GS/Cobrança Automatizada/main_cobranca.py
@@ -1,3 +1,3 @@
 from cobranca_automatizada import cobranca_automatizada
 
-cobranca_automatizada(0) #o parametro é a quantidade de dias (positivo é quantos dias faltam e negativo dias de atraso)
+cobranca_automatizada(2) #o parametro é a quantidade de dias (positivo é quantos dias faltam e negativo dias de atraso)
---
+++ GS/Cobrança Automatizada/teste.py
@@ -0,0 +1,4 @@
+from ler_credenciais import ler_credenciais_db, ler_credenciais_remet
+
+print(ler_credenciais_remet())
+print(ler_credenciais_db)
